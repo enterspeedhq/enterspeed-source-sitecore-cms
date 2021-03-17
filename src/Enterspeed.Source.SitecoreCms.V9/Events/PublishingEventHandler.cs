18d4d41578385991ddb409583427144210999339
Kristian Elmholt Kjær <kek@novicell.dk>
2021-03-17 08:22:18 +0100

    Publish Event handler now handles the basic publishing logic

--- src/Enterspeed.Source.SitecoreCms.V9/Events/PublishingEventHandler.cs
+++ src/Enterspeed.Source.SitecoreCms.V9/Events/PublishingEventHandler.cs
@@ -1,42 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Enterspeed.Source.SitecoreCms.V9.Mappers;
+using Enterspeed.Source.SitecoreCms.V9.Models;
 using Sitecore.Abstractions;
 using Sitecore.Data.Events;
+using Sitecore.Data.Items;
 using Sitecore.Events;
 using Sitecore.Globalization;
 using Sitecore.Publishing;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using Version = Sitecore.Data.Version;
 
 namespace Enterspeed.Source.SitecoreCms.V9.Events
 {
     public class PublishingEventHandler
     {
         private readonly BaseLanguageManager _languageManager;
+        private readonly BaseItemManager _itemManager;
         private readonly SitecoreContentEntityModelMapper _mapper;
 
         public PublishingEventHandler(
             BaseLanguageManager languageManager,
+            BaseItemManager itemManager,
             SitecoreContentEntityModelMapper mapper)
         {
             _languageManager = languageManager;
+            _itemManager = itemManager;
             _mapper = mapper;
         }
 
-        public void OnItemPublished(object sender, EventArgs args)
+        public void OnComplete(object sender, EventArgs args)
         {
-            if (args is PublishEndRemoteEventArgs remoteEventArgs)
+            var publishOptions =
+                Event.ExtractParameter<IEnumerable<DistributedPublishOptions>>(args, 0)?.ToList() ??
+                new List<DistributedPublishOptions>();
+            if (publishOptions.Any() == false)
             {
-                Language language = _languageManager.GetLanguage(remoteEventArgs.LanguageName);
+                return;
             }
-            else
+
+            foreach (var publishOption in publishOptions)
             {
-                Publisher publisher = Event.ExtractParameter(args, 0) as Publisher;
-                if (publisher == null)
+                // For whatever reason, if the target database is not web, we skip this
+                if (publishOption.TargetDatabaseName.Equals("web", StringComparison.OrdinalIgnoreCase) == false)
                 {
-                    return;
+                    continue;
                 }
 
-                Language language = publisher.Options.Language;
+                string culture = publishOption.LanguageName;
             }
+        }
+
+        public void OnItemProcessed(object sender, EventArgs args)
+        {
+            PublishItemContext context = args is ItemProcessedEventArgs itemProcessedEventArgs
+                ? itemProcessedEventArgs.Context
+                : null;
+
+            if (context == null)
+            {
+                return;
+            }
+
+            if (context.Action != PublishAction.DeleteTargetItem &&
+                context.Action != PublishAction.PublishVersion)
+            {
+                return;
+            }
+
+            Language language = context.PublishOptions.Language;
+
+            Item item = _itemManager.GetItem(context.ItemId, language, Version.Latest, context.PublishHelper.Options.TargetDatabase);
+            if (item == null || item.Versions.Count == 0)
+            {
+                return;
+            }
+
+            SitecoreContentEntity sitecoreContentEntity = _mapper.Map(item, item.Language.Name);
         }
     }
 }
--- src/Enterspeed.Source.SitecoreCms.V9/Mappers/SitecoreContentEntityModelMapper.cs
+++ src/Enterspeed.Source.SitecoreCms.V9/Mappers/SitecoreContentEntityModelMapper.cs
@@ -1,6 +1,8 @@
 ﻿using Enterspeed.Source.SitecoreCms.V9.Models;
 using Enterspeed.Source.SitecoreCms.V9.Services;
+using Sitecore.Abstractions;
 using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
 
 namespace Enterspeed.Source.SitecoreCms.V9.Mappers
 {
@@ -8,13 +10,16 @@ namespace Enterspeed.Source.SitecoreCms.V9.Mappers
     {
         private readonly IContentIdentityService _contentIdentityService;
         private readonly IEnterspeedPropertyService _enterspeedPropertyService;
+        private readonly BaseLinkManager _linkManager;
 
         public SitecoreContentEntityModelMapper(
             IContentIdentityService contentIdentityService,
-            IEnterspeedPropertyService enterspeedPropertyService)
+            IEnterspeedPropertyService enterspeedPropertyService,
+            BaseLinkManager linkManager)
         {
             _contentIdentityService = contentIdentityService;
             _enterspeedPropertyService = enterspeedPropertyService;
+            _linkManager = linkManager;
         }
 
         public SitecoreContentEntity Map(Item item, string culture)
@@ -22,6 +27,12 @@ namespace Enterspeed.Source.SitecoreCms.V9.Mappers
             var output = new SitecoreContentEntity();
 
             output.Id = _contentIdentityService.GetId(item, culture);
+            output.Type = item.TemplateName;
+            output.ParentId = _contentIdentityService.GetId(item.Parent, culture);
+            output.Url = _linkManager.GetItemUrl(item, new ItemUrlBuilderOptions
+            {
+                AlwaysIncludeServerUrl = true
+            });
 
             output.Properties = _enterspeedPropertyService.GetProperties(item, culture);
 
