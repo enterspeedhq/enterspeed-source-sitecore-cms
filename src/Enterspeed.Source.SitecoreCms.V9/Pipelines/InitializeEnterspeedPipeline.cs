afd02a38804e6cc99dd77eba25ba3f69ff3d0763
Kristian Elmholt Kjær <kek@novicell.dk>
2021-03-24 10:10:40 +0100

    Added init pipeline shell + moved config into one file

---
+++ src/Enterspeed.Source.SitecoreCms.V9/Pipelines/InitializeEnterspeedPipeline.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.Pipelines;
+
+namespace Enterspeed.Source.SitecoreCms.V9.Pipelines
+{
+    public class InitializeEnterspeedPipeline
+    {
+        public void Process(PipelineArgs args)
+        {
+        }
+    }
+}
