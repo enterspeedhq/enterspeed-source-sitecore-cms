df28ec11c25fedb64c58fe53314d4371e44b3700
Mathias Hove <mathiashove02@gmail.com>
2022-07-07 15:27:08 +0300

    Jobfactory added

---
+++ src/Enterspeed.Source.SitecoreCms.V8/Data/Models/EnterspeedContentState.cs
@@ -0,0 +1,8 @@
+﻿namespace Enterspeed.Source.SitecoreCms.V8.Data.Models
+{
+    public enum EnterspeedContentState
+    {
+        Publish,
+        Preview
+    }
+}
---
+++ src/Enterspeed.Source.SitecoreCms.V8/Factories/IEnterspeedJobFactory.cs
@@ -0,0 +1,12 @@
+﻿using Enterspeed.Source.SitecoreCms.V8.Data.Models;
+using Sitecore.Data.Items;
+
+namespace Enterspeed.Source.SitecoreCms.V8.Factories
+{
+    public interface IEnterspeedJobFactory
+    {
+        EnterspeedJob GetPublishJob(Item content, string culture, EnterspeedContentState state);
+        EnterspeedJob GetDeleteJob(Item content, string culture, EnterspeedContentState state);
+        EnterspeedJob GetFailedJob(EnterspeedJob job, string exception);
+    }
+}
