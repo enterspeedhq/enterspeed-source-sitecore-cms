2ca0cebf8897a15fa3d96f0dc3d2ceaf4e7a984e
Mathias Hove <mathiashove02@gmail.com>
2022-07-07 16:15:06 +0300

    Added enterspeed job repository

---
+++ src/Enterspeed.Source.SitecoreCms.V8/Data/Repositories/EnterspeedJobRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Enterspeed.Source.SitecoreCms.V8.Data.Models;
+
+namespace Enterspeed.Source.SitecoreCms.V8.Data.Repositories
+{
+    public class EnterspeedJobRepository : IEnterspeedJobRepository
+    {
+        public EnterspeedJobRepository()
+        {
+        }
+
+        public IList<EnterspeedJob> GetFailedJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EnterspeedJob> GetFailedJobs(IList<string> entityIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EnterspeedJob> GetPendingJobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<EnterspeedJob> GetOldProcessingTasks(int olderThanMinutes = 60)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(IList<EnterspeedJob> jobs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IList<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Enterspeed.Source.SitecoreCms.V8/Data/Repositories/IEnterspeedJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Enterspeed.Source.SitecoreCms.V8.Data.Models;
+
+namespace Enterspeed.Source.SitecoreCms.V8.Data.Repositories
+{
+    public interface IEnterspeedJobRepository
+    {
+        IList<EnterspeedJob> GetFailedJobs();
+        IList<EnterspeedJob> GetFailedJobs(IList<string> entityIds);
+        IList<EnterspeedJob> GetPendingJobs();
+        IList<EnterspeedJob> GetOldProcessingTasks(int olderThanMinutes = 60);
+        void Save(IList<EnterspeedJob> jobs);
+        void Delete(IList<int> ids);
+    }
+}
