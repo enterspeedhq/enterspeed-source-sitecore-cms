c1df0c5420a492638146d8b69ade7863b56a60c2
Mathias Hove <mathiashove02@gmail.com>
2022-07-24 22:32:03 +0200

    Enterspeed dashboard

--- src/Enterspeed.Source.SitecoreCms.V8/Controllers/EnterspeedController.cs
+++ src/Enterspeed.Source.SitecoreCms.V8/Controllers/EnterspeedController.cs
@@ -1,32 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
-using Enterspeed.Source.Sdk.Api.Models;
-using Enterspeed.Source.Sdk.Api.Services;
-using Enterspeed.Source.SitecoreCms.V8.Extensions;
+using Enterspeed.Source.SitecoreCms.V8.Data.Repositories;
 using Enterspeed.Source.SitecoreCms.V8.Models;
-using Enterspeed.Source.SitecoreCms.V8.Models.Configuration;
-using Enterspeed.Source.SitecoreCms.V8.Models.Mappers;
-using Enterspeed.Source.SitecoreCms.V8.Services.Contracts;
-using Sitecore.Abstractions;
-using Sitecore.Data;
-using Sitecore.Data.Items;
-using Sitecore.Globalization;
-using Version = Sitecore.Data.Version;
 
 namespace Enterspeed.Source.SitecoreCms.V8.Controllers
 {
     public class EnterspeedController : Controller
     {
-        public EnterspeedController()
+        private readonly IEnterspeedJobRepository _enterspeedJobRepository;
+
+        public EnterspeedController(IEnterspeedJobRepository enterspeedJobRepository)
         {
-         
+            _enterspeedJobRepository = enterspeedJobRepository;
         }
 
         public ActionResult Index()
         {
-            return View();
+            var failedJobs = _enterspeedJobRepository.GetFailedJobs();
+            var pendingJobs = _enterspeedJobRepository.GetPendingJobs(1000000);
+
+            var vm = new EnterspeedDashboardViewModel()
+            {
+                FailedJobs = failedJobs,
+                PendingJobs = pendingJobs
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ src/Enterspeed.Source.SitecoreCms.V8/Models/EnterspeedDashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Enterspeed.Source.SitecoreCms.V8.Data.Models;
+
+namespace Enterspeed.Source.SitecoreCms.V8.Models
+{
+    public class EnterspeedDashboardViewModel
+    {
+        public IList<EnterspeedJob> PendingJobs { get; set; }
+        public IList<EnterspeedJob> FailedJobs { get; set; }
+    }
+}
